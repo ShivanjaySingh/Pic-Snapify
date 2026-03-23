e3e443a9d59efad580860450f5a4776584753edc
ShivanjaySingh <120712668+ShivanjaySingh@users.noreply.github.com>
2026-03-23 21:07:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,10 +26,10 @@ app =Flask(__name__)
 app.secret_key = "tyu653azedr3wyhv!554"
 
 # MySQL configurations
-app.config['MYSQL_HOST'] = 'database-1.c9qaeokiumh0.eu-north-1.rds.amazonaws.com'
+app.config['MYSQL_HOST'] = 'database-2.c9qaeokiumh0.eu-north-1.rds.amazonaws.com'
 app.config['MYSQL_USER'] = 'admin'               
-app.config['MYSQL_PASSWORD'] = 'P8RU4ZCxYaPZGewd3WtB'   
-app.config['MYSQL_DB']  = 'PicSnapifyInitialDB'   
+app.config['MYSQL_PASSWORD'] = 'database-2345'   
+app.config['MYSQL_DB']  = 'PicSnapifyDB'   
 
 # ADD THIS LINE FOR SSL
 app.config['MYSQL_CUSTOM_OPTIONS'] = {"ssl": {"ca": "/etc/mysql/certs/global-bundle.pem"}}
